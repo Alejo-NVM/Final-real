1e5dec1186a288de60c0162d74423a388ad6d038
Alejo-NVM <alejomartinez00@hotmail.com>
2025-12-10 16:29:02 -0300

    Modal para errores
    Sacamos la confirmación horrible de windows

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import ListadoProductos from "./ListadoProductos";
 import FormularioProducto from "./FormularioProducto";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Modal from "react-modal";
 
 function App() {
   const [productos, setProductos] = useState([]);
@@ -13,6 +14,8 @@ function App() {
   const [modalAbierto, setModalAbierto] = useState(false);
   const [productoEditando, setProductoEditando] = useState(null);
   const [filtros, setFiltros] = useState({ tipo: "", nombre: "" });
+const [modalConfirmOpen, setModalConfirmOpen] = useState(false);
+const [idAEliminar, setIdAEliminar] = useState(null);
 
   // Inicializar datos
   async function cargarDatos() {
@@ -58,16 +61,23 @@ function App() {
     setModalAbierto(true);
   }
 
-  async function eliminar(id) {
-    if (!window.confirm("¿Eliminar producto?")) return;
-    try {
-      await api.delete(`/productos/${id}`);
-      toast.success("Producto eliminado");
-      cargarDatos();
-    } catch {
-      toast.error("Error al eliminar");
-    }
+  function eliminar(id) {
+  setIdAEliminar(id);
+  setModalConfirmOpen(true);
+}
+async function confirmarEliminacion() {
+  try {
+    await api.delete(`/productos/${idAEliminar}`);
+    toast.success("Producto eliminado");
+    cargarDatos();
+  } catch {
+    toast.error("Error al eliminar");
+  } finally {
+    setModalConfirmOpen(false);
+    setIdAEliminar(null);
   }
+}
+
 
   return (
     <>
@@ -81,6 +91,9 @@ function App() {
       </button>
 
       <FiltroProductos tipos={tipos} onFiltrar={setFiltros} />
+      <button className="btn btn-refresh mb-3 ms-3" onClick={cargarDatos}>
+        Actualizar
+      </button>
 
       <ListadoProductos
         productos={productosFiltrados}
@@ -95,6 +108,32 @@ function App() {
         producto={productoEditando}
         refrescar={cargarDatos}
       />
+      <Modal
+  isOpen={modalConfirmOpen}
+  onRequestClose={() => setModalConfirmOpen(false)}
+  overlayClassName="modal-fondo"
+  className="modal-contenido"
+>
+  <h4>Confirmar eliminación</h4>
+  <p>¿Estás seguro de que querés eliminar este producto?</p>
+
+  <div className="text-end mt-3">
+    <button
+      className="btn btn-secondary me-2"
+      onClick={() => setModalConfirmOpen(false)}
+    >
+      Cancelar
+    </button>
+
+    <button
+      className="btn btn-danger"
+      onClick={confirmarEliminacion}
+    >
+      Eliminar
+    </button>
+  </div>
+</Modal>
+
     </div>
     </>
   );
